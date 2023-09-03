2bdab25ede19d27bfc70492c1387d9c8637d398c
JuanCamiloYepesAlbarracin <jyepes511@unab.edu.co>
2023-09-03 12:42:16 -0500

    ShaopCart005 - feat Barra de tareas perfeccionada sujeta a cambios

--- Styles/index.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("nav ul li a").forEach(function(element) {
-    element.addEventListener("mouseover", function() {
-        this.style.transform = "scale(1.1)";
-    });
-    
-    element.addEventListener("mouseout", function() {
-        this.style.transform = "scale(1)";
-    });
-});
